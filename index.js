7bbcd5bab93ab34ff99417c9ce55c5d0b68caa7b
Kim <kmdnpm@gmail.com>
2022-05-26 20:53:59 -0400

    inventory altered

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function newImage(url, left, bottom){
+
+
+function newImage(url, left, bottom) {
     let object = document.createElement('img')
     object.src = url
     object.style.position = 'fixed'
@@ -8,11 +10,18 @@ function newImage(url, left, bottom){
     return object
 }
 
-function newItem(url, left, bottom){
+function newItem(url, left, bottom) {
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', function () {
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
 }
 
-function newInventory(){
+
+function newInventory() {
     let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
